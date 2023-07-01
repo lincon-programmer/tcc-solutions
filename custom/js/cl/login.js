15104fa9ef97ea3e81a7658f23088d49e267c64d
lincon-programmer <62950796+lincon-programmer@users.noreply.github.com>
2023-07-01 22:30:08 +0000

    feat: criando arquivos de mock para permitir que o front possa usar um arquivo demock estando em ambiente fora de produção sem precisar acessar informações reais direto da api.

--- custom/js/cl/login.js
+++ custom/js/cl/login.js
@@ -1,1 +1,32 @@
-let button_send;
+let button_send = document.getElementById('button_send');
+let email = document.getElementById('email');
+let password = document.getElementById('senha');
+
+/**  
+<div class="mb-3">
+  <label class="form-label">Validation States (lite)</label>
+  <input type="text" class="form-control is-valid is-valid-lite mb-2" placeholder="Valid State..">
+  <input type="text" class="form-control is-invalid is-invalid-lite" placeholder="Invalid State..">
+</div>
+*/
+
+button_send.addEventListener('click', function() {
+  password = password.value;
+  email = email.value;
+
+  if(
+    email != undefined &&
+    password != undefined &&
+    email != "" &&
+    password != ""
+    ) {
+      alert("tudo ok")
+    } else {
+      alert("tem campo faltando")
+    }
+
+  console.log({
+    email,
+    password
+  });
+})
